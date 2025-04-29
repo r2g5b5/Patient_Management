9b2038484544265e1ec6a012c903b4204d5b7421
h.rahmani <programmer4906@gmail.com>
2025-04-29 16:14:37 +0330

    init

---
+++ billing-service/src/main/java/com/example/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,26 @@
+package com.example.billingservice.grpc;
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(BillingRequest request, StreamObserver<BillingResponse> responseObserver) {
+        log.info("Creating billing account for request {} ", request.toString());
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("1234")
+                .setStatus("ACTIVE")
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
